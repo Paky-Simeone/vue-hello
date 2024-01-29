d4b9fc526c730b1d647a4cea555374d2f8a735c0
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-29 15:12:32 +0100

    problema con il bous

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Ciao Gigio , come stai?'
-      };
-    }
+        message: 'Ciao Gigio , come stai?',
+        logo: "https://www.raiplay.it/cropgd/1440x810/dl/img/2020/04/09/1586424957500_2048x1152.jpg",
+    };
+    },
   }).mount('#app');
