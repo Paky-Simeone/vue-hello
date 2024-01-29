ea7c97c8ebe253047c13e2ac01ba7366421ceff7
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-29 15:16:46 +0100

    bonus fatto

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const { createApp } = Vue
     data() {
       return {
         message: 'Ciao Gigio , come stai?',
-        logo: "https://www.raiplay.it/cropgd/1440x810/dl/img/2020/04/09/1586424957500_2048x1152.jpg",
+        logo: "./img/gigi2.png",
     };
     },
   }).mount('#app');
